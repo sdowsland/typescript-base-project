84cdcc68cd7c9d3c503f479e840eb67ee6b546c1
Stephen Dowsland <nsd61@MacBook-Pro-3.local>
2017-07-23 08:51:28 +0100

    Initial Project

---
+++ bin/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t sdowsland/typescript-base-project .
---
+++ bin/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -it --rm -v $PWD:/usr/local/app sdowsland/typescript-base-project bash
